7f9b5d2b852bd28b8184a2d1453f998cd3b94cee
Lya Carolina <lyacarolina94@gmail.com>
2024-01-15 14:18:13 -0300

    Alteração do exercício para incluir número de chances

--- exercicios_livro/exerc1.py
+++ exercicios_livro/exerc1.py
@@ -3,21 +3,21 @@ import random
 
 def guessing_game():
     number = random.randint(0,100)
-
     
-    while True:
-        
+
+    for x in range(1,4):
         user_number = int(input("Adivinhe um número entre 0 e 100: "))
-        
-        if user_number == number:
-            print("Número correto!!")
-            break
-            
+                
         if user_number > number:
             print("Menos")
 
-        if user_number < number:
+        elif user_number < number:
             print("Mais")
             
+        else: 
+            print("Número correto!!")
+            return
             
+    print(f"Você não tem mais tentativas. A resposta é {number}.")
+        
 guessing_game()
